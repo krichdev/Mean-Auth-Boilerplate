ac8f9e404d01c88af7571783a7d2bf60ba9f5264
krichdev <kgrichardson2@gmail.com>
2017-03-20 21:25:34 -0700

    MEAN auth starter code

---
+++ public/app/controllers.js
@@ -0,0 +1,42 @@
+angular.module('AppCtrl', ['AppServices'])
+.controller('SignupCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
+    $scope.user = {
+        email: '',
+        password: ''
+    };
+    $scope.userSignup = function() {
+        // to implement
+        $http.post('/api/users', $scope.user).then(function success(res) {
+        $state.go("home");
+        }, function error(err) {
+        console.log("Error", err)
+        })
+    };
+}])
+.controller('LoginCtrl', ['$scope', '$http', '$state', 'Auth', function($scope, $http, $state, Auth) {
+    $scope.user = {
+        email: '',
+        password: ''
+    };
+    $scope.userLogin = function() {
+        // to implement
+        $http.post("/api/auth", $scope.user).then(function success(res) {
+        Auth.saveToken(res.data.token);
+        $state.go("home")
+        }, function error(err) {
+            console.log("Yo dawg")
+        })
+        }
+}])
+.controller('NavCtrl', ['$scope', 'Auth', '$location', function($scope, Auth, $location) {
+  $scope.isLoggedIn = function() {
+    return Auth.isLoggedIn();
+  }
+  $scope.logout = function() {
+    // to implement
+    console.log("Before Logout", Auth.getToken());
+    Auth.removeToken();
+    console.log("After Logout", Auth.getToken());
+    $location.path("/login");
+  };
+}])
